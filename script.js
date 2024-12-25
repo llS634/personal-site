38f5d255591facdbe991eff2a542b28bfa2ac5a2
Elis <eliza.sashenkova@gmail.com>
2024-12-25 18:16:18 +0700

    fix: menu hide and link activity

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var currentUrl = window.location.href;
+    var currentPath = window.location.pathname.split('/').pop() || 'index.html';
     var links = document.querySelectorAll('.nav-link');
 
     links.forEach(function (link) {
-        if (currentUrl.includes(link.dataset.page)) {
+        if (link.dataset.page === currentPath) {
             link.classList.add('disabled-link');
             link.removeAttribute('target');
         } else {
-            link.addEventListener('click', function(event) {
-                if (this.target === '_blank') {
-                    return;
-                }
+            link.addEventListener('click', function (event) {
+                if (this.target === '_blank') return;
                 event.preventDefault();
                 window.location.href = this.getAttribute('href');
             });
@@ -23,7 +21,6 @@ document.getElementById('menuLink').addEventListener('click', function(event) {
     document.getElementById('menu').classList.toggle('open');
 });
 
-
 document.querySelectorAll('.contact-button').forEach(button => {
     button.addEventListener('click', function(event) {
       event.preventDefault();
@@ -31,3 +28,15 @@ document.querySelectorAll('.contact-button').forEach(button => {
       menu.classList.toggle('show');
     });
   });
+
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('menu');
+    const menuLink = document.getElementById('menuLink');
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnMenuLink = menuLink.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnMenuLink) {
+        menu.classList.remove('open');
+        menu.classList.remove('show');
+    }
+});
